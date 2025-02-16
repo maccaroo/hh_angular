52f91ec938c832e81cb4e9c433695b4617701ab2
Maciej Krzysik <maccaroo@gmail.com>
2025-02-16 21:35:42 +0800

    Configure to use API for auth

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from "../../../environments/environment";
 
 @Injectable({
     providedIn: "root"
@@ -10,16 +13,26 @@ export class AuthService {
 
     private token: string | null = null;
 
+    private apiUrl = `${environment.apiUrl}/auth/login`;
+
+    constructor(private http: HttpClient) {}
+
     login(username: string, password: string): Observable<boolean> {
-        // Simulate an API call for authentication
-        if (username == 'admin' && password == 'password') {
-            this.token = 'dummy-token'; // TODO: Replace with real token from the API
-            this.isAuthenticatedSubject.next(true);
-            return this.isAuthenticated$;
-        } else {
-            this.isAuthenticatedSubject.next(false);
-            return this.isAuthenticated$;
-        }
+        return this.http.post<{ token: string }>(this.apiUrl, { username, password }).pipe(
+            map(response => {
+
+                console.log('API Response:', response);  
+
+                if (response.token) {
+                    this.token = response.token;
+                    this.isAuthenticatedSubject.next(true);
+                    return true;
+                } else {
+                    this.isAuthenticatedSubject.next(false);
+                    return false;
+                }
+            })
+        )
     }
 
     logout(): void {
