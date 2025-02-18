9a5275502405f0f6b904abb233bcbbcf4acd52df
Maciej Krzysik <maccaroo@gmail.com>
2025-02-18 22:13:35 +0800

    Add unit tests for data source and data point services

---
+++ src/app/core/services/data-point.servoce.spec.ts
@@ -0,0 +1,51 @@
+import { TestBed } from '@angular/core/testing';
+import { DataPointService } from './data-point.service';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { PagedResponse } from '../models/paged-response';
+import { DataPoint } from '../models/data-point';
+import { environment } from '../../../environments/environment';
+
+describe('DataPointService', () => {
+    let service: DataPointService;
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [DataPointService, provideHttpClient(), provideHttpClientTesting()]
+        });
+        service = TestBed.inject(DataPointService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should fetch data points', () => {
+        const mockResponse: PagedResponse<DataPoint> = {
+            offset: 0,
+            limit: 5,
+            total: 1,
+            data: [{
+                id: 1,
+                dataSourceId: 1,
+                createdAt: new Date().toISOString(),
+                value: 'test'
+             }]
+        };
+        service.getDataPoints(1, 0, 5).subscribe(response => {
+            expect(response.data.length).toBe(1);
+            expect(response.total).toBe(1);
+            expect(response.data[0].value).toBe('test');
+        });
+        
+        const req = httpMock.expectOne(`${environment.apiUrl}/datasources/1/datapoints?offset=0&limit=5`);
+        expect(req.request.method).toBe('GET');
+        req.flush(mockResponse);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+});
---
+++ src/app/core/services/data-source.service.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing';
+import { DataSourceService } from './data-source.service';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { PagedResponse } from '../models/paged-response';
+import { DataSource } from '../models/data-source';
+import { environment } from '../../../environments/environment';
+
+describe('DataSourceService', () => {
+    let service: DataSourceService;
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [DataSourceService, provideHttpClient(), provideHttpClientTesting()]
+        });
+        service = TestBed.inject(DataSourceService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should fetch data sources', () => {
+        const mockResponse: PagedResponse<DataSource> = {
+            offset: 0,
+            limit: 5,
+            total: 1,
+            data: [{
+                id: 1,
+                name: 'Test Data Source',
+                description: 'This is a test data source',
+                createdAt: new Date().toISOString(),
+                createdByUserId: 1,
+                dataType: 'string'
+             }]
+        };
+
+        service.getDataSources(0, 5).subscribe(response => {
+            expect(response.data.length).toBe(1);
+            expect(response.total).toBe(1);
+            expect(response.data[0].name).toBe('Test Data Source');
+        });
+
+        const req = httpMock.expectOne(`${environment.apiUrl}/datasources?offset=0&limit=5`);
+        expect(req.request.method).toBe('GET');
+        req.flush(mockResponse);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+});
