0ac3be9143eb136269c177fbcb48deba232496e8
Maciej Krzysik <maccaroo@gmail.com>
2025-02-18 21:38:21 +0800

    Add unit tests for data source detail and list components.

---
+++ src/app/features/data-source/data-source-detail.component.spec.ts
@@ -0,0 +1,63 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DataSourceDetailComponent } from './data-source-detail.component';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { DataSourceService } from '../../core/services/data-source.service';
+import { DataPointService } from '../../core/services/data-point.service';
+
+describe('DataSourceDetailComponent', () => {
+    let component: DataSourceDetailComponent;
+    let fixture: ComponentFixture<DataSourceDetailComponent>;
+    let dataSourceServiceSpy: jasmine.SpyObj<DataSourceService>;
+    let dataPointServiceSpy: jasmine.SpyObj<DataPointService>;
+
+    beforeEach(async () => {
+        dataSourceServiceSpy = jasmine.createSpyObj('DataSourceService', ['getDataSource']);
+        dataPointServiceSpy = jasmine.createSpyObj('DataPointService', ['getDataPoints']);
+
+        dataSourceServiceSpy.getDataSource.and.returnValue(of({
+            id: 1,
+            name: 'Test Data Source',
+            description: 'This is a test data source',
+            createdAt: new Date().toISOString(),
+            createdByUserId: 1,
+            dataType: 'string'
+        }));
+        dataPointServiceSpy.getDataPoints.and.returnValue(of({
+            offset: 0,
+            limit: 10,
+            total: 0,
+            data: []
+        }));
+
+        await TestBed.configureTestingModule({
+            imports: [DataSourceDetailComponent],
+            providers: [
+                provideHttpClient(),
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            paramMap: convertToParamMap({ id: '1' })
+                        }
+                    }
+                },
+                { provide: DataSourceService, useValue: dataSourceServiceSpy },
+                { provide: DataPointService, useValue: dataPointServiceSpy }
+            ]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(DataSourceDetailComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should load data source details on init', () => {
+        expect(component.dataSource).toBeDefined();
+    });
+});
---
+++ src/app/features/data-source/data-source-list.component.spec.ts
@@ -0,0 +1,51 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DataSourceListComponent } from './data-source-list.component';
+import { DataSourceService } from '../../core/services/data-source.service';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+
+
+describe('DataSourceListComponent', () => {
+    let component: DataSourceListComponent;
+    let fixture: ComponentFixture<DataSourceListComponent>;
+    let dataSourceServiceSpy: jasmine.SpyObj<DataSourceService>;
+
+    beforeEach(async () => {
+        dataSourceServiceSpy = jasmine.createSpyObj('DataSourceService', ['getDataSources']);
+
+        await TestBed.configureTestingModule({
+            imports: [DataSourceListComponent],
+            providers: [
+                provideHttpClient(),
+                { provide: DataSourceService, useValue: dataSourceServiceSpy },
+                { provide: ActivatedRoute, useValue: { paramMap: of(convertToParamMap({ id: 1 })) } }
+            ]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(DataSourceListComponent);
+        component = fixture.componentInstance;
+        dataSourceServiceSpy.getDataSources.and.returnValue(of({
+            offset: 0,
+            limit: 10,
+            total: 0,
+            data: []
+        }));
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should load data sources on init', () => {
+        expect(dataSourceServiceSpy.getDataSources).toHaveBeenCalled();
+    });
+
+    it('should change page and reload data sources', () => {
+        spyOn(component, 'loadDataSources');
+        component.onPageChange(2);
+        expect(component.currentPage).toBe(2);
+        expect(component.loadDataSources).toHaveBeenCalled();
+    });
+});
