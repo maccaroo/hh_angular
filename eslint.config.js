850dbfbe9ece07a8bc757457bac5d6d2b331bc9a
Maciej Krzysik <maccaroo@gmail.com>
2025-02-19 09:23:31 +0800

    Add linting

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslintPlugin from "@typescript-eslint/eslint-plugin";
+import eslintParser from "@typescript-eslint/parser";
+import angularEslintPlugin from "@angular-eslint/eslint-plugin";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: eslintParser,
+      parserOptions: {
+        project: "tsconfig.json",
+        tsconfigRootDir: process.cwd(),
+        createDefaultProgram: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": eslintPlugin,
+      "@angular-eslint": angularEslintPlugin,
+    },
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        { type: "attribute", prefix: "app", style: "camelCase" },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        { type: "element", prefix: "app", style: "kebab-case" },
+        ],
+      "no-extra-semi": "warn",
+    },
+  }
+];
