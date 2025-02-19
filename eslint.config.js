068c176e53fbcd99107563efec7128f2d8a8c9d3
Maciej Krzysik <maccaroo@gmail.com>
2025-02-19 09:47:41 +0800

    Require 4 spaces on indent

--- eslint.config.js
+++ eslint.config.js
@@ -3,30 +3,31 @@ import eslintParser from "@typescript-eslint/parser";
 import angularEslintPlugin from "@angular-eslint/eslint-plugin";
 
 export default [
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: eslintParser,
-      parserOptions: {
-        project: "tsconfig.json",
-        tsconfigRootDir: process.cwd(),
-        createDefaultProgram: true,
-      },
-    },
-    plugins: {
-      "@typescript-eslint": eslintPlugin,
-      "@angular-eslint": angularEslintPlugin,
-    },
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        { type: "attribute", prefix: "app", style: "camelCase" },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        { type: "element", prefix: "app", style: "kebab-case" },
-        ],
-      "no-extra-semi": "warn",
-    },
-  }
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: eslintParser,
+            parserOptions: {
+                project: "tsconfig.json",
+                tsconfigRootDir: process.cwd(),
+                createDefaultProgram: true,
+            },
+        },
+        plugins: {
+            "@typescript-eslint": eslintPlugin,
+            "@angular-eslint": angularEslintPlugin,
+        },
+        rules: {
+            "@angular-eslint/directive-selector": [
+                "error",
+                { type: "attribute", prefix: "app", style: "camelCase" },
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                { type: "element", prefix: "app", style: "kebab-case" },
+            ],
+            "no-extra-semi": "warn",
+            "indent": ["warn", 4],
+        },
+    }
 ];
